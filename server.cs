f70afbd55bbc458d385fb02c5a9f6613f10b053e
qu-ota <34490428+qu-ota@users.noreply.github.com>
2019-01-08 07:22:45 -0500

    edit code

--- server.cs
+++ server.cs
@@ -1,13 +1,10 @@
 //HMM POKE AND DIRECT MESSAGE
 //By Dominoes
 
-if(isFile("Add-Ons/System_ReturnToBlockland/server.cs") || isFile("Add-Ons/System_BlocklandGlass/server.cs"))
+if(isFile("Add-Ons/System_ReturnToBlockland/server.cs"))
 {
-	if(!$RTB::RTBR_ServerControl_Hook)
-	{
-		RTB_registerPref("Enable Poke?","Poke and PM","Pref::PokePM::EnablePoke","bool","Server_PokePM",1,0,0);
-		RTB_registerPref("Enable PM?","Poke and PM","Pref::PokePM::EnablePM","bool","Server_PokePM",1,0,0);
-	}
+	RTB_registerPref("Enable Poke?","Poke and PM","Pref::PokePM::EnablePoke","bool","Server_PokePM",1,0,0);
+	RTB_registerPref("Enable PM?","Poke and PM","Pref::PokePM::EnablePM","bool","Server_PokePM",1,0,0);
 }
 else
 {
@@ -15,7 +12,7 @@ else
 	$Pref::PokePM::EnablePM = 1; //Allow users to private message others?
 }
 
-function reallowPokePM(%client)
+function reallowPoke(%client)
 {
 	%client.sentPoke = 0;
 }
@@ -31,38 +28,39 @@ function serverCmdPoke(%cl, %id)
 	
 	if($Pref::PokePM::EnablePoke == 0)
 		return messageClient(%cl,'',"\c7The server host has \c0disabled \c7poking other users.");
-	if(%target.allowPoke == 0)
-		return messageClient(%cl,'',"\c6" @ %t.name SPC "\c7has disabled poking.");
+	//if(%target.allowPoke == 0)
+		//return messageClient(%cl,'',"\c6" @ %t.name SPC "\c7has disabled poking.");
 	if(!isObject(%t))
 		return messageClient(%cl,'',"\c7Your input \c6(" @ %t.name @ ") \c7doesn't seem to be a user that exists.");
 	if(%cl.sentPoke == 1)
-		return messageClient(%cl,'',"\c7You've already poked someone, try again in a few seconds. \c6(10 second cooldown)");
+		return messageClient(%cl,'',"\c7You've already poked someone, try again in a few seconds. \c6(" @ mFloatLength(3 - ($Sim::Time - %cl.lastPokeTime),2) SPC "seconds left)");
 	if(%t.name $= %cl.name)
 		return messageClient(%cl,'',"\c7You can't poke yourself, you dingus.");
 	
 	commandToClient(%t,'MessageBoxOK',"Poke Notification",%cl.name SPC "has poked you. Close this dialogue box when you're back.");
-	%cl.play2D(errorsound);
+	%t.play2D(errorsound);
 	messageClient(%t,'',"\c6" @ %cl.name SPC "\c7has poked you.");
 	messageClient(%cl,'',"\c7You have successfully poked \c6" @ %t.name @ "\c7.");
 	%cl.sentPoke = 1;
-	schedule(10000, 0, reallowPoke, %cl);
+	%cl.lastPokeTime = $Sim::Time;
+	schedule(3000, 0, reallowPoke, %cl);
 }
 
-function serverCmdAllowPoke(%cl)
-{
-	if($Pref::PokePM::EnablePoke == 0)
-		return messageClient(%cl,'',"\c7The server host has \c0disabled \c7poking.");
-	if(%cl.allowPoke == 0)
-	{
-		messageClient(%cl,'',"\c7You have \c3enabled \c7poking; other users can now send you poke notifications.");
-		%cl.allowPoke = 1;
-	}
-	else
-	{
-		messageClient(%cl,'',"\c7You have \c0disabled \c7poking; other users can no longer send you poke notifications.");
-		%cl.allowPoke = 0;
-	}
-}
+//function serverCmdAllowPoke(%cl)
+//{
+	//if($Pref::PokePM::EnablePoke == 0)
+		//return messageClient(%cl,'',"\c7The server host has \c0disabled \c7poking.");
+	//if(%cl.allowPoke == 0)
+	//{
+		//messageClient(%cl,'',"\c7You have \c3enabled \c7poking; other users can now send you poke notifications.");
+		//%cl.allowPoke = 1;
+	//}
+	//else
+	//{
+		//messageClient(%cl,'',"\c7You have \c0disabled \c7poking; other users can no longer send you poke notifications.");
+		//%cl.allowPoke = 0;
+	//}
+//}
 
 function serverCmdPM(%cl,%id,%c1,%c2,%c3,%c4,%c5,%c6,%c7,%c8,%c9,%c10,%c11,%c12,%c13,%c14,%c16,%c17,%c18,%c19,%c20)
 {
@@ -70,12 +68,12 @@ function serverCmdPM(%cl,%id,%c1,%c2,%c3,%c4,%c5,%c6,%c7,%c8,%c9,%c10,%c11,%c12,
 	
 	if($Pref::PokePM::EnablePM == 0)
 		return messageClient(%cl,'',"\c7The host has \c0disabled \c7private messaging between others.");
-	if(%t.allowPM == 0)
-		return messageClient(%cl,'',"\c7The user you've entered has disabled receiving PMs.");
+	//if(%t.allowPM == 0)
+		//return messageClient(%cl,'',"\c7The user you've entered has disabled receiving PMs.");
 	if(!isObject(%t))
 		return messageClient(%cl,'',"\c7Your input \c6(" @ %t.name @ ") \c7doesn't seem to be a user that exists.");
 	if(%cl.sentPM == 1)
-		return messageClient(%cl,'',"\c7You've already sent a private message, please wait a bit before trying again. \c6(10 second cooldown)");
+		return messageClient(%cl,'',"\c7You've already sent a private message, please wait a bit before trying again. \c6(" @ mFloatLength(3 - ($Sim::Time - %cl.lastPMTime),2) SPC "seconds left)");
 	if(%t.name $= %cl.name)
 		return messageClient(%cl,'',"\c7You can't whisper to yourself. Sounds like mental insanity...don't do that.");
 	
@@ -87,25 +85,31 @@ function serverCmdPM(%cl,%id,%c1,%c2,%c3,%c4,%c5,%c6,%c7,%c8,%c9,%c10,%c11,%c12,
 		}
 	}
 	
+	if(%pm $="")
+		return messageClient(%cl,'',"\c7You didn't put in a message.");
+	%msgLen = strLen(%pm);
+	if(%msgLen > 124)
+		return messageClient(%cl,'',"\c7The message you put is \c6" @ %msgLen - 124 SPC "\c7character(s) too long.");
 	%pm = stripMLControlChars(trim(%pm));
-	messageClient(%cl,'',"\c3" @ %cl.name SPC "\c7[PM to" @ %t.name @ "]\c6:" SPC %pm);
-	messageClient(%t,'',"\c3" @ %cl.name SPC "\c7[PM from" @ %cl.name @ "]\c6:" SPC %pm);
+	messageClient(%cl,'',"\c7" @ %cl.clanPrefix @ "\c3" @ %cl.name @ "\c7" @ %cl.clanSuffix SPC "\c7[Whisper to" SPC %t.name @ "]\c6:" SPC %pm);
+	messageClient(%t,'',"\c7" @ %cl.clanPrefix @ "\c3" @ %cl.name @ "\c7" @ %cl.clanSuffix SPC "\c7[Whisper]\c6:" SPC %pm);
 	%cl.sentPM = 1;
-	schedule(10000,0,reallowPM,%cl);
+	%cl.lastPMTime = $Sim::Time;
+	schedule(3000,0,reallowPM,%cl);
 }
 
-function serverCmdAllowPM(%cl)
-{
-	if($Pref::PokePM::EnablePM == 0)
-		return messageClient(%cl,'',"\c7The server host has \c0disabled \c7private messaging between others.");
-	if(%cl.allowPM == 0)
-	{
-		messageClient(%cl,'',"\c7You have \c3enabled \c7the receiving of private messages; other users can now send you private messages.");
-		%cl.allowPM = 1;
-	}
-	else
-	{
-		messageClient(%cl,'',"\c7You have \c0disabled \c7the receiving of private messages; other users can no longer send them to you.");
-		%cl.allowPoke = 0;
-	}
-}
+//function serverCmdAllowPM(%cl)
+//{
+	//if($Pref::PokePM::EnablePM == 0)
+		//return messageClient(%cl,'',"\c7The server host has \c0disabled \c7private messaging between others.");
+	//if(%cl.allowPM == 0)
+	//{
+		//messageClient(%cl,'',"\c7You have \c3enabled \c7the receiving of private messages; other users can now send you private messages.");
+		//%cl.allowPM = 1;
+	//}
+	//else
+	//{
+		//messageClient(%cl,'',"\c7You have \c0disabled \c7the receiving of private messages; other users can no longer send them to you.");
+		//%cl.allowPoke = 0;
+	//}
+//}
